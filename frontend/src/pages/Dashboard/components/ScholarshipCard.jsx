434572ee09d898c455071212c02e56d2647fb821
AndrewGlago <andrewglago1@gmail.com>
2021-10-18 19:20:15 +0000

    reuse sc card

--- frontend/src/pages/Dashboard/components/ScholarshipCard.jsx
+++ frontend/src/pages/Dashboard/components/ScholarshipCard.jsx
@@ -1,11 +1,16 @@
+import { useContext } from "react";
+import { ModalContext } from "../../../components/Modals/ModalContext";
+
 import { MdPeopleAlt, MdOutlineHideSource } from "react-icons/md";
 import { AiOutlineStar } from "react-icons/ai";
 import { BsExclamationTriangleFill } from "react-icons/bs";
 
-const ScholarshipCard = () => {
+const ScholarshipCard = ({ applied }) => {
+  const { modalData, setModalData } = useContext(ModalContext);
+
   return (
     <div className="w-full border rounded-sm flex flex-col p-4 gap-4">
-      <div className="flex">
+      <div className="flex gap-2">
         <div className="w-12 h-12">
           <img src="https://photos.angel.co/startups/i/5001845-166baf15b842a06817010b6dc196fda3-medium_jpg.jpg?buster=1582753841" />
         </div>
@@ -21,8 +26,22 @@ const ScholarshipCard = () => {
       <div className="w-full">
         <div className="w-full border flex p-2 rounded-[4px] justify-between items-center">
           <p>Software engineer, Server Side Engineering</p>
-          <div className="border rounded-[4px] border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white">
-            <button className="py-1 px-3">Apply</button>
+          <div
+            className={`border rounded-[4px] border-blue-500  hover:bg-blue-500 hover:text-white ${
+              applied && "bg-blue-500 text-white"
+            }`}
+          >
+            <button
+              className={`py-1 px-3 ${applied && "cursor-default"}`}
+              onClick={() => {
+                setModalData({
+                  modalShow: true,
+                  modalType: "apply",
+                });
+              }}
+            >
+              {applied ? "Applied" : "Apply"}
+            </button>
           </div>
         </div>
       </div>
