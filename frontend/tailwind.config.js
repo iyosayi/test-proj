01284ab04328f09f56778a43e10c7cfcac93da9c
AndrewGlago <andrewglago1@gmail.com>
2021-10-18 19:19:22 +0000

    palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,10 +7,15 @@ module.exports = {
       colors: {
         primary: "#0071ff",
         overlay: "hsl(213deg 100% 29% / 78%)",
+        header: "hsl(213deg 100% 29%)",
         button: "rgb(0 67 148)",
         text: {
           header: "#1a3651",
           paragraph: "#40515b",
+        },
+        modal: {
+          infoBackground: "#f2f4f7",
+          backdrop: "rgba(0,0,0,.57)",
         },
       },
     },
