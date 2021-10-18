e98e7374809d384e5a50b935e7f242eb59afd8a2
AndrewGlago <andrewglago1@gmail.com>
2021-10-18 19:20:37 +0000

    move nav into header

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,34 +1,32 @@
-import { FaBell } from "react-icons/fa";
-import { IoMdPerson } from "react-icons/io";
-import logo from "@assets/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { ImBooks } from "react-icons/im";
+import { FaInbox } from "react-icons/fa";
 
 const Nav = () => {
+  const path = useLocation().pathname;
+
   return (
-    <nav className="h-20 bg-overlay opacity-90 text-white px-4 py-4">
-      <div className="max-w-[1800px] m-auto h-full flex justify-between">
-        <Link to="/" className="flex h-full gap-2">
-          <img src={logo} className="" />
-          <p className="self-center font-medium text-xl">Scholars</p>
-        </Link>
-        <div className="flex items-center text-2xl gap-1">
-          <div
-            role="button"
-            className="flex items-center p-3 gap-2 rounded-full transition-all ease-in-out duration-300 hover:bg-overlay hover:text-blue-200"
-          >
-            <FaBell />
-          </div>
-          <div
-            role="button"
-            className="flex items-center p-3 gap-2 rounded-xl transition-all ease-in-out duration-300 hover:bg-overlay"
-          >
-            <div className="bg-gray-400 p-1 rounded-full">
-              <IoMdPerson />
-            </div>
-            <p className="text-sm">Andrew Glago</p>
-          </div>
-        </div>
-      </div>
+    <nav className="fixed flex justify-center p-4 bottom-0 bg-[#f8f8fd] w-full xl:w-auto xl:top-32 xl:bottom-auto xl:flex-col">
+      <Link
+        to="/"
+        role="button"
+        className={`${
+          path === "/" && "bg-overlay text-white"
+        } p-4 rounded-lg text-center text-sm lg:text-base`}
+      >
+        <ImBooks className="lg:text-4xl m-auto" />
+        Scholarships
+      </Link>
+      <Link
+        to="/applied"
+        role="button"
+        className={`${
+          path === "/applied" && "bg-overlay text-white"
+        } p-4 rounded-lg text-center text-sm lg:text-base`}
+      >
+        <FaInbox className="lg:text-4xl m-auto" />
+        Applied
+      </Link>
     </nav>
   );
 };
