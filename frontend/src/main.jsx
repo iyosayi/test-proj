b0cd14bcd68aa7e0a64d936e332c08c3b8a49a6e
AndrewGlago <andrewglago1@gmail.com>
2021-10-19 02:10:19 +0000

    rq setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import App from "./App";
 import { ModalContextProvider } from "./components/Modals/ModalContext";
+
 import "./index.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <ModalContextProvider>
-      <App />
-    </ModalContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <ModalContextProvider>
+        <App />
+      </ModalContextProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
