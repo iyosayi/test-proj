7382c1d912849df01ff0d03399daa2f94d6ae447
AndrewGlago <andrewglago1@gmail.com>
2021-10-19 18:11:37 +0000

    onboarding responsiveness

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useRef } from "react";
 import logo from "@assets/logo.png";
 import person from "@assets/person.png";
 import LoginForm from "./components/LoginForm";
 
 const Login = () => {
+  const loginRef = useRef(null);
+
+  useEffect(() => {
+    setTimeout(
+      () =>
+        loginRef.current.scrollIntoView({
+          behavior: "smooth",
+        }),
+      1000
+    );
+  }, []);
+
   return (
-    <div className="h-screen w-full grid grid-cols-10">
+    <div className="h-screen w-full md:grid md:grid-cols-10">
       <div className="h-full w-full col-span-2 relative">
         <div className="absolute h-full w-full z-10 bg-overlay" />
         <div className="absolute top-0 w-full text-white z-20 flex flex-col p-2 gap-5">
@@ -12,14 +25,14 @@ const Login = () => {
             <img src={logo} className="h-12 w-12 mr-2" />
             <p>Scholars</p>
           </div>
-          <div className="inline-block m-auto text-xl p-3">
+          <div className="inline-block md:m-auto text-xl p-3">
             <h1>You're just a few clicks away from endless possibilties.</h1>
           </div>
         </div>
         <img src={person} className="absolute bottom-0" />
       </div>
       <div className="col-span-8 flex">
-        <div className="w-2/3 m-auto">
+        <div className="w-2/3 m-auto py-16" ref={loginRef}>
           <h1 className="text-text-header text-5xl mb-20">Login</h1>
           <section className="space-y-5 mb-10">
             <h2 className="text-text-paragraph text-3xl">
